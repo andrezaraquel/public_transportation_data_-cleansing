88f907089d7bed03d693e816c5bd6e7a165bc75b
andrezaraquel <andreza.queiroz@copin.ufcg.edu.br>
2019-10-21 14:10:57 -0300

    modified graph hopper MM

---
+++ map-matching/matching-web/src/main/java/com/graphhopper/matching/cli/MatchCommand.java
@@ -0,0 +1,168 @@
+package com.graphhopper.matching.cli;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.graphhopper.GraphHopper;
+import com.graphhopper.PathWrapper;
+import com.graphhopper.matching.Observation;
+import com.graphhopper.matching.State;
+import com.graphhopper.matching.gpx.Gpx;
+import com.graphhopper.matching.EdgeMatch;
+import com.graphhopper.matching.MapMatching;
+import com.graphhopper.matching.MatchResult;
+import com.graphhopper.reader.osm.GraphHopperOSM;
+import com.graphhopper.routing.AlgorithmOptions;
+import com.graphhopper.routing.Path;
+import com.graphhopper.routing.util.FlagEncoder;
+import com.graphhopper.routing.util.HintsMap;
+import com.graphhopper.routing.weighting.FastestWeighting;
+import com.graphhopper.util.*;
+import com.graphhopper.util.gpx.GpxFromInstructions;
+import io.dropwizard.cli.Command;
+import io.dropwizard.setup.Bootstrap;
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class MatchCommand extends Command {
+
+    public MatchCommand() {
+        super("match", "map-match one or more gpx files");
+    }
+
+    @Override
+    public void configure(Subparser subparser) {
+        subparser.addArgument("gpx")
+                .type(File.class)
+                .required(true)
+                .nargs("+")
+                .help("GPX file");
+        subparser.addArgument("--instructions")
+                .type(String.class)
+                .required(false)
+                .setDefault("")
+                .help("Locale for instructions");
+        subparser.addArgument("--max_visited_nodes")
+                .type(Integer.class)
+                .required(false)
+                .setDefault(1000);
+        subparser.addArgument("--gps_accuracy")
+                .type(Integer.class)
+                .required(false)
+                .setDefault(40);
+        subparser.addArgument("--transition_probability_beta")
+                .type(Double.class)
+                .required(false)
+                .setDefault(2.0);
+    }
+
+    @Override
+    public void run(Bootstrap bootstrap, Namespace args) {
+        CmdArgs graphHopperConfiguration = new CmdArgs();
+        graphHopperConfiguration.put("graph.location", "../graph-cache");
+        GraphHopper hopper = new GraphHopperOSM().init(graphHopperConfiguration);
+        hopper.getCHFactoryDecorator().setEnabled(false);
+        System.out.println("loading graph from cache");
+        hopper.load(graphHopperConfiguration.get("graph.location", "graph-cache"));
+
+        FlagEncoder firstEncoder = hopper.getEncodingManager().fetchEdgeEncoders().get(0);
+        AlgorithmOptions opts = AlgorithmOptions.start().
+                algorithm(Parameters.Algorithms.DIJKSTRA_BI).traversalMode(hopper.getTraversalMode()).
+                weighting(new FastestWeighting(firstEncoder)).
+                maxVisitedNodes(args.getInt("max_visited_nodes")).
+                // Penalizing inner-link U-turns only works with fastest weighting, since
+                // shortest weighting does not apply penalties to unfavored virtual edges.
+                hints(new HintsMap().put("weighting", "fastest").put("vehicle", firstEncoder.toString())).
+                build();
+        MapMatching mapMatching = new MapMatching(hopper, opts);
+        mapMatching.setTransitionProbabilityBeta(args.getDouble("transition_probability_beta"));
+        mapMatching.setMeasurementErrorSigma(args.getInt("gps_accuracy"));
+
+        StopWatch importSW = new StopWatch();
+        StopWatch matchSW = new StopWatch();
+
+        Translation tr = new TranslationMap().doImport().getWithFallBack(Helper.getLocale(args.getString("instructions")));
+        final boolean withRoute = !args.getString("instructions").isEmpty();
+        XmlMapper xmlMapper = new XmlMapper();
+        
+        
+        String folderPath = String.valueOf(args.getAttrs().get("gpx")).replace("[", "").replace("]", "");
+        File folder = new File(folderPath);
+        for (File gpxFile : folder.listFiles()) {
+            try {
+                importSW.start();
+                Gpx gpx = xmlMapper.readValue(gpxFile, Gpx.class);
+                if (gpx.trk == null) {
+                    throw new IllegalArgumentException("No tracks found in GPX document. Are you using waypoints or routes instead?");
+                }
+                if (gpx.trk.size() > 1) {
+                    throw new IllegalArgumentException("GPX documents with multiple tracks not supported yet.");
+                }
+                List<Observation> measurements = gpx.trk.get(0).getEntries();
+                importSW.stop();
+                matchSW.start();
+                MatchResult mr = mapMatching.doWork(measurements);
+                matchSW.stop();
+                System.out.println(gpxFile);
+                System.out.println("\tmatches:\t" + mr.getEdgeMatches().size() + ", gps entries:" + measurements.size());
+                System.out.println("\tgpx length:\t" + (float) mr.getGpxEntriesLength() + " vs " + (float) mr.getMatchLength());
+                generateOutFileWithEdgeIds(gpxFile.getAbsolutePath().substring(0, gpxFile.getAbsolutePath().length()-4) + "_mr.txt", mr);
+                
+                String outFile = gpxFile.getAbsolutePath() + ".res.gpx";
+                System.out.println("\texport results to:" + outFile);
+
+                PathWrapper pathWrapper = new PathWrapper();
+                new PathMerger().doWork(pathWrapper, Collections.singletonList(mr.getMergedPath()), hopper.getEncodingManager(), tr);
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(outFile))) {
+                    long time = gpx.trk.get(0).getStartTime()
+                            .map(Date::getTime)
+                            .orElse(System.currentTimeMillis());
+                    writer.append(GpxFromInstructions.createGPX(pathWrapper.getInstructions(), gpx.trk.get(0).name != null ? gpx.trk.get(0).name : "", time, hopper.hasElevation(), withRoute, true, false, Constants.VERSION, tr));
+                }
+            } catch (Exception ex) {
+                importSW.stop();
+                matchSW.stop();
+                System.err.println("Problem with file " + gpxFile);
+                ex.printStackTrace(System.err);
+            }
+        }
+
+        System.out.println("gps import took:" + importSW.getSeconds() + "s, match took: " + matchSW.getSeconds());
+    }
+
+	private void generateOutFileWithEdgeIds(String outFile, MatchResult mr) {
+		System.out.println("\texport results to:" + outFile);
+		
+		
+	      try (BufferedWriter writer = new BufferedWriter(new FileWriter(outFile))) {	   
+	    	  writer.append("edgeID;edgeName;edgeDistance");
+	    	  
+	    	  for (EdgeMatch em : mr.getEdgeMatches()) {
+	    		  writer.newLine();
+	    		  writer.append(""+ em.getEdgeState().getEdge());
+	    		  writer.append(";"+ em.getEdgeState().getName());
+	    		  writer.append(";"+ em.getEdgeState().getDistance());
+	    		  
+	    		  // TODO andreza - mudei
+//	    		  for ( State state : em.getStates() ) {
+//		    		  System.out.print(state.getQueryResult().getSnappedPoint().getLat() + ",");
+//		    		  System.out.println(state.getQueryResult().getSnappedPoint().getLon());
+//		    		  
+//		    	  }
+	    	  }
+	    	  
+			
+	          
+	      } catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
